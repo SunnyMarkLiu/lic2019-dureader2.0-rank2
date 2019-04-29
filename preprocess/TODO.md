13c254a2a72a3560c28c9aa8377f42c6c3b3b5c6
SunnyMarkLiu <sunnymarkliu101@gmail.com>
2019-04-29 13:50:46 +0800

    code reformat

--- preprocess/TODO.md
+++
@@ -1,7 +0,0 @@
-# TODO
-## 1.text_cleaning.py
-- 百度词向量的词典 all_entities_dict_baidu_embed.txt 用于结巴分词提高覆盖率
-
-## 3.gen_mrc_dataset.py
-- 标题是否参与检索 (Done)
-- 如果答案直接在原文中，则直接定位（Done，有效）
--- preprocess/run_5.remove_low_ceil_metric.sh
+++ preprocess/run_5.remove_low_ceil_metric.sh
@@ -8,7 +8,7 @@ target_dir="../input/${data_version}/final_mrc_dataset/trainset/"
 
 min_ceil_rouge=0    # 过滤 ceil rouge 小于该阈值的样本
 
-#------------------------ generate train mrc dataset ------------------------
+#------------------------ remove train low ceil rouge-l samples ------------------------
 nohup cat ${source_dir}split_search_00 |python 5.remove_low_ceil_metric.py ${min_ceil_rouge} > ${target_dir}split_search_00 2>&1 &
 nohup cat ${source_dir}split_search_01 |python 5.remove_low_ceil_metric.py ${min_ceil_rouge} > ${target_dir}split_search_01 2>&1 &
 nohup cat ${source_dir}split_search_02 |python 5.remove_low_ceil_metric.py ${min_ceil_rouge} > ${target_dir}split_search_02 2>&1 &
@@ -39,7 +39,7 @@ nohup cat ${source_dir}split_zhidao_11 |python 5.remove_low_ceil_metric.py ${min
 nohup cat ${source_dir}split_zhidao_12 |python 5.remove_low_ceil_metric.py ${min_ceil_rouge} > ${target_dir}split_zhidao_12 2>&1 &
 nohup cat ${source_dir}split_zhidao_13 |python 5.remove_low_ceil_metric.py ${min_ceil_rouge} > ${target_dir}split_zhidao_13 2>&1 &
 
-#------------------------ generate dev mrc dataset ------------------------
+#------------------------ remove dev low ceil rouge-l samples ------------------------
 source_dir="../input/${data_version}/mrc_dataset/devset/"
 target_dir="../input/${data_version}/final_mrc_dataset/devset/"
 
